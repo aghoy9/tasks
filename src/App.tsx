b5b74df744361a42c7855e7f58da3d2f2838f8de
Alex <aghoy@udel.edu>
2024-02-13 17:27:45 -0500

    Added requirements for task 3 plus an image

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
-
+import logo from "./AB.png";
+import { Button, Col, Container, Row } from "react-bootstrap";
 function App(): JSX.Element {
     return (
         <div className="App">
+            <h1>this is header text</h1>
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
@@ -11,6 +13,31 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Also im Alex Hoy, Hello World.
             </p>
+            <Container>
+                <Row>
+                    <Col>
+                        <img src={logo} alt="One Angry Bird" />
+                    </Col>
+                    <Col>
+                        Why this bird is angry:
+                        <ol>
+                            <li>Eggs got taken</li>
+                            <li>He is red</li>
+                            <li>He is frowning</li>
+                        </ol>
+                        <div
+                            style={{
+                                height: "30px",
+                                width: "30px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
         </div>
     );
 }
